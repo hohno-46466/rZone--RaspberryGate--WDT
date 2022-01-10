2d7f3ab394248bb3d30958784db451190dfaf2dc
hohno-46466 <hohno.46466@gmail.com>
2022-01-10 21:08:19 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- Arduino/sketch_20220110_test1/sketch_20220110_test1.ino
+++ Arduino/sketch_20220110_test1/sketch_20220110_test1.ino
@@ -2,10 +2,18 @@
 // sketch_20220110_test1
 //
 
+#define PIN_LED	(13)
+
 void setup() {
   // setup function is here
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
 }
 
 void loop() {
   // loop function is here
+  digitalWrite(PIN_LED, HIGH);
+  delay(300);
+  digitalWrite(PIN_LED, LOW);
+  delay(700);
 }
