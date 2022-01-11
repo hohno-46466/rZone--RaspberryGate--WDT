8424858af50bc7eded76e3e0a46fcded65cdc1eb
hohno-46466 <hohno.46466@gmail.com>
2022-01-11 20:10:03 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

---
+++ Arduino/sketch_20220111_newWDT/mydefs.h
@@ -0,0 +1,25 @@
+#ifndef __MYDEFS_H__
+#define __MYDEFS_H__
+
+//
+// mydefs.h
+//
+
+// ---------------------------------------------------------
+
+// History:
+// First version: Tue Jan 11 13:36:55 JST 2022 by @hohno_at_kuimc
+
+
+// ---------------------------------------------------------
+
+const int PIN_0 = 0;
+const int PIN_1 = 1;
+const int PIN_2 = 2;
+const int PIN_3 = 3;
+const int PIN_4 = 4;
+const int PIN_5 = 5;
+
+// ---------------------------------------------------------
+
+#endif // __MYDEFS_H__
